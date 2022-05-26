79687476fe0041b96a63b7be1b9cb2c0552c1afd
Wufisher <1105889424@qq.com>
2022-05-26 14:15:49 +0800

    client

--- privateServer/Client/Main.java
+++ privateServer/Client/Main.java
@@ -3,8 +3,9 @@ package Client;
 public class Main {
     public static void main(String[] args) {
         try {
+            String context = "";
             PriClient client = new PriClient("10.249.8.149", 9999);
-            client.sendMessage("hello \n world");
+            client.sendMessage(context);
             client.sendRun();
             client.waitOver();
             client.sendGet();
