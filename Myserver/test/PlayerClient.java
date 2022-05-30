c0ab7491766219259656d8ddd23abe592321a0f5
Wufisher <1105889424@qq.com>
2022-05-30 14:48:55 +0800

    pc

--- Myserver/test/PlayerClient.java
+++ Myserver/test/PlayerClient.java
@@ -314,6 +314,7 @@ public class PlayerClient {
 
     public void sendContent(String content, Socket socket) {
         try {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
             pw.println(content);
             pw.flush();
             System.out.println("send:" + content);
@@ -326,7 +327,7 @@ public class PlayerClient {
     public String getContent(Socket socket) {
         String content = "";
         try {
-
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             content = br.readLine();
             System.out.println("收到" + content);
         } catch (Exception e) {
